0c1b3e499a7ede1393bbb4d8744b6c29c38682ad
JAHNAVI BANOTH <125012875+jahnavi854janu@users.noreply.github.com>
2026-08-07 12:08:36 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,9 +13,9 @@ const Index = () => {
       <Navigation />
       <Hero />
       <About />
-      <Education />
-      <Projects />
       <Skills />
+      <Projects />
+      <Education />
       <Contact />
       <Footer />
     </div>
