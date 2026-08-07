f574c7856533a2b8b5dbda9954b3c9ab049393a3
JAHNAVI BANOTH <125012875+jahnavi854janu@users.noreply.github.com>
2026-08-07 12:39:16 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,6 +4,7 @@ import About from '@/components/About';
 import Education from '@/components/Education';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
+import Certificates from '@/components/Certificates';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 
